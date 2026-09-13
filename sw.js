19f7120dfc488aa6f1bc614a06b67537df15626b
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-13 10:49:54 +0200

    Met a jour cache Chrono V4.4.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chrono-carnet-v4.3.0';
-const APP=['/','/index.html','/css/app.css?v=4.3.0','/js/app.js?v=4.3.0','/manifest.webmanifest'];
+const CACHE='chrono-carnet-v4.4.2';
+const APP=['/','/index.html','/css/app.css?v=4.4.1','/js/app.js?v=4.4.1','/js/v44.js?v=3','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
