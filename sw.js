116c7b8e102849e6c2d3b75835a182ca0a355a56
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-21 19:52:07 +0200

    Refresh offline cache v6.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chrono-carnet-v5.9.0";
+const CACHE = "chrono-carnet-v6.0.0";
 
 const APP = [
   "/",
@@ -7,7 +7,7 @@ const APP = [
   "/css/app.css?v=4.4.1",
   "/css/clarity.css?v=2",
 
-  "/js/app.js?v=5.9",
+  "/js/app.js?v=6.0",
   "/js/v44.js?v=2",
   "/js/ccf-input-guard.js?v=1",
 
