2446f43c603526a0c15c3a205425827f605ca4ef
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-24 17:50:30 +0200

    Refresh offline cache for chrono pyramid

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chrono-carnet-v6.6.1";
+const CACHE = "chrono-carnet-v46";
 
 const APP = [
   "/",
@@ -7,7 +7,7 @@ const APP = [
   "/css/app.css?v=4.4.1",
   "/css/clarity.css?v=2",
 
-  "/js/app.js?v=6.6",
+  "/js/app.js?v=46",
   "/js/v44.js?v=2",
   "/js/ccf-input-guard.js?v=1",
   "/js/qrcode.min.js?v=1",
