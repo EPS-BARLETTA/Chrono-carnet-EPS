6fd3c48657d643f910aa4ca1baa03bae9bfbdc7e
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-24 19:39:41 +0200

    Refresh offline cache for carnet opening fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chrono-carnet-v51";
+const CACHE = "chrono-carnet-v52";
 
 const APP = [
   "/",
@@ -7,7 +7,7 @@ const APP = [
   "/css/app.css?v=4.4.1",
   "/css/clarity.css?v=2",
 
-  "/js/app.js?v=51",
+  "/js/app.js?v=52",
   "/js/v44.js?v=2",
   "/js/ccf-input-guard.js?v=1",
   "/js/qrcode.min.js?v=1",
