91c1dcf3f2e6c2a2abfb4f4d162244235b016bf9
EPS-BARLETTA <j.barletta@vauban.lu>
2026-09-24 19:34:16 +0200

    Refresh offline cache for compact table export

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chrono-carnet-v50";
+const CACHE = "chrono-carnet-v51";
 
 const APP = [
   "/",
@@ -7,7 +7,7 @@ const APP = [
   "/css/app.css?v=4.4.1",
   "/css/clarity.css?v=2",
 
-  "/js/app.js?v=50",
+  "/js/app.js?v=51",
   "/js/v44.js?v=2",
   "/js/ccf-input-guard.js?v=1",
   "/js/qrcode.min.js?v=1",
